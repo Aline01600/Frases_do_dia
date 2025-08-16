328525b195ea8045fd4b0414c6aa7abe23c5a21b
Aline Soares <117992962+Aline01600@users.noreply.github.com>
2025-08-16 12:23:44 -0300

    Elementos Básicos

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
-import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart'; // contém todos os widgets e classes que seguem o design Material Design do Google
 
-class MyApp extends StatelessWidget{
-  @verride
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Material APP', 
-      home: Scaffold(
+void main() => runApp(MyApp()); // => = {}, notação pra quando se tem uma unica linha
+
+class MyApp extends StatelessWidget { //a maioria dos elementos da interface do usuário (UI) é uma classe que herda de Widget stateless não mudam suas carcteristicas, ou stateful mudam suas caracteristicas com mudança de estdo.
+  @override
+  Widget build(BuildContext context) { //O build é o método mais importante de qualquer widget no Flutter. É ele que descreve como o widget deve ser desenhado na tela.
+    return MaterialApp( //widget principal que configura o ambiente para o aplicativo
+      debugShowCheckedModeBanner: false,
+      title: 'Frases do dia',
+      home: Scaffold( //widget que cria um layout visual básico e comum de um aplicativo Material Design. Ele fornece os "espaços" pré-definidos para você colocar seu conteúdo
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: const Text("Frases do Dia"),
+          backgroundColor: Colors.greenAccent,
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ) 
-      )
-    )
+        body: Text("Corpo da página"),
+        bottomNavigationBar: const BottomAppBar(
+          color: Colors.greenAccent,
+          child: Text("botões aqui")
+        ),
+      ),
+    );
   }
 }
