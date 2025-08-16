68b4c0d4f23ac4f1e0647d7723eff8d04d37678a
Aline Soares <117992962+Aline01600@users.noreply.github.com>
2025-08-16 14:44:23 -0300

    Containers, colunas,linhas e imagens

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
-import 'package:flutter/material.dart'; // contém todos os widgets e classes que seguem o design Material Design do Google
+import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp()); // => = {}, notação pra quando se tem uma unica linha
+void main() => runApp(FrasesDoDia());
 
-class MyApp extends StatelessWidget { //a maioria dos elementos da interface do usuário (UI) é uma classe que herda de Widget stateless não mudam suas carcteristicas, ou stateful mudam suas caracteristicas com mudança de estdo.
-  @override
-  Widget build(BuildContext context) { //O build é o método mais importante de qualquer widget no Flutter. É ele que descreve como o widget deve ser desenhado na tela.
-    return MaterialApp( //widget principal que configura o ambiente para o aplicativo
-      debugShowCheckedModeBanner: false,
-      title: 'Frases do dia',
-      home: Scaffold( //widget que cria um layout visual básico e comum de um aplicativo Material Design. Ele fornece os "espaços" pré-definidos para você colocar seu conteúdo
-        appBar: AppBar(
-          title: const Text("Frases do Dia"),
-          backgroundColor: Colors.greenAccent,
+class FrasesDoDia extends StatefulWidget {
+@override
+_FrasesDoDiaState createState() => _FrasesDoDiaState();
+}
+
+class _FrasesDoDiaState extends State<FrasesDoDia> {
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Frases do dia',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.green,
+      ),
+      bottomNavigationBar: const BottomAppBar(color: Colors.green),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.yellow),
         ),
-        body: Text("Corpo da página"),
-        bottomNavigationBar: const BottomAppBar(
-          color: Colors.greenAccent,
-          child: Text("botões aqui")
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("imagens/logo.png"),
+            const Text(
+              "Clique abaixo para gerar uma nova frase!",
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 17),
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 }
